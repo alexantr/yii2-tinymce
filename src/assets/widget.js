43a970b80ec0f43ec1be1456b1e2bbd225eb2500
biladina <nabil_munawar@protonmail.ch>
2022-09-22 15:12:21 +0700

    add keyup function to accomodate input in bootstrap modal with ajax form

--- src/assets/widget.js
+++ src/assets/widget.js
@@ -11,9 +11,17 @@ alexantr.tinyMceWidget = (function ($) {
             prevInstance.remove();
         }
         options.selector = '#' + inputId;
+        // add keyup function to accomodate input in bootstrap modal with ajax form
+        options.setup = function(editor) {
+            editor.on('keyup', function () {
+                tinymce.triggerSave(); 
+            });
+        };
         tinymce.init(options);
-        $('#' + inputId).closest('form').on('beforeValidate', function () {
-            tinymce.triggerSave();
-        });
+
+        // this below function can safe to remove, because has been accomodated with keyup function
+        // $('#' + inputId).closest('form').on('beforeValidate', function () {
+        //     tinymce.triggerSave();
+        // });
     }
 })(jQuery);
